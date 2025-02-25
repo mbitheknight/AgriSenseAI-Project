973a726abb9b8070ea1b3dd0beec3fa7862e234d
Knight Mbithe Wambua <knightmbithe17@gmail.com>
2025-02-25 19:36:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+# main.py
 from api import app  # Import FastAPI app instance
 import uvicorn
-import os
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))  # Get the port from environment variable
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    uvicorn.run(app, host="127.0.0.1", port=8000)
